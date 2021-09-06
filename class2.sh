b2f8765f9d38de9a5cd6712018431353c82837bb
anikakash <anik15-14210@diu.edu.bd>
2021-09-06 19:36:24 +0600

    learn commenting is shell and work with variable

---
+++ class2.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+# above line is the dir of the bash 
+# for know your bash location you just need to type which bash command on your terminal
+# echo is used for printing line on terminal
+# Let's lalk about variable. System Variable are Written in Capitial Letter. 
+# user define are written any of two small or capitial
+
+
+echo $BASH
+
+echo The Version of the bash sheel is $BASH_VERSION
+echo The home directory is $HOME
+echo Current Working Directory is $PWD
+# user define variable
+
+name=Manjaro
+echo My Name Is $name
